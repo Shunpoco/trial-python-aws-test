a5465bff061c0c0c09b570aed253e10110410e81
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-09 11:35:03 +0900

    test without mock class

--- s3_test.py
+++ s3_test.py
@@ -103,7 +103,7 @@ class StubS3(S3):
 
         self.stubber = Stubber(self.client)
 
-def test_S3():
+def test_StubS3():
     s3 = StubS3()
 
     response = {
@@ -132,6 +132,41 @@ def test_S3():
     s3.stubber.add_response("list_objects", response, expected_params)
 
     with s3.stubber:
+        service_response = s3.list_objects(bucket="test-bucket")
+
+    assert service_response == response
+
+def test_S3():
+    s3 = S3()
+
+    stubber = Stubber(s3.client)
+
+    response = {
+        "IsTruncated": False,
+        "Name": "test-bucket",
+        "MaxKeys": 1000,
+        "Prefix": "",
+        "Contents": [{
+            "Key": "test.txt",
+            "ETag": '"abc123"',
+            "StorageClass": "STANDARD",
+            "LastModified": datetime.datetime(2016, 1, 20, 22, 9),
+            "Owner": {"ID": "abc123", "DisplayName": "myname"},
+            "Size": 14814,
+        }],
+        "EncodingType": "url",
+        "ResponseMetadata": {
+            "RequestId": "abc123",
+            "HTTPStatusCode": 200,
+            "HostId": "abc123",
+        },
+        "Marker": ""
+    }
+ 
+    expected_params = {"Bucket": ANY}
+    stubber.add_response("list_objects", response, expected_params)
+
+    with stubber:
         service_response = s3.list_objects(bucket="test-bucket")
 
     assert service_response == response
